f8c38ceee8aedb4fcf63904ac08964b3a1c42d13
Chandni Kumari <108781610+chandnii96@users.noreply.github.com>
2023-02-25 21:31:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+
+st.title(":red[Bebiotic] data app  :grinning:")
+st.header("Learn-Explore-Develop")
+#st.subheader("")
+
+
+
+btn_click = st.button("Click me")
+
+if btn_click == True:
+    st.subheader("Hello")
+
